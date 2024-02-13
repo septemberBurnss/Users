729e1c9bff0d3ac57ec2c2f4f468d4dde785b809
Nikita Avraimov <n.avraimov@gmail.com>
2024-02-13 17:03:23 +0100

    Retrofit

--- app/src/main/java/nikita/awraimow/githubusers/users/data/network/GithubService.kt
+++ app/src/main/java/nikita/awraimow/githubusers/users/data/network/GithubService.kt
@@ -3,13 +3,20 @@ package nikita.awraimow.githubusers.users.data.network
 import nikita.awraimow.githubusers.users.data.model.GithubUserDetailsResponseItem
 import nikita.awraimow.githubusers.users.data.model.GithubUsersResponseItem
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface GithubService {
 
+    @Headers("Authorization: Bearer $token")
     @GET("users")
     suspend fun getUsers(): List<GithubUsersResponseItem>
 
+    @Headers("Authorization: Bearer $token")
     @GET("user/{userId}")
     suspend fun getUser(@Path("userId") userId: Int): GithubUserDetailsResponseItem
+
+    private companion object {
+        private const val token = ""
+    }
 }
